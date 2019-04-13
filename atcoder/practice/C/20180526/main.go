ba15b5c07ca2ac02d96c7df7f3b7daf182a3e0c3
smith-30 <parkour12019@gmail.com>
2019-04-13 19:37:48 +0900

    add: practice

--- atcoder/practice/C/20180526/main.go
+++ atcoder/practice/C/20180526/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -11,26 +11,49 @@ func main() {
 	str := SingleStr()
 	strs := strings.Split(str, "")
 
-	cs := make([]int, 0, n)
-	l := len(strs)
-	for idx, _ := range strs {
-		var c int
-		for i := 0; i < idx; i++ {
-			if strs[i] == "W" {
-				c++
-			}
+	r := make([]int, 0, n)
+	l := make([]int, 0, n)
+	var nw, ne, wc, ec int
+
+	for _, v := range strs {
+		nw = wc
+		ne = ec
+
+		if v == "W" {
+			wc++
+		} else {
+			ec++
 		}
-		right := idx + 1
-		for i := right; i < l; i++ {
-			if strs[i] == "E" {
-				c++
-			}
+
+		if nw != wc {
+			l = append(l, wc-1)
+		} else {
+			l = append(l, wc)
 		}
-		cs = append(cs, c)
+
+		if ne != ec {
+			r = append(r, ec-1)
+		} else {
+			r = append(r, ec)
+		}
 	}
 
-	sort.Sort(sort.IntSlice(cs))
-	fmt.Printf("%#v\n", cs[0])
+	fmt.Printf("%#v\n", l)
+	fmt.Printf("%#v\n", r)
+
+	var min int
+	var once sync.Once
+	for i := 0; i < n; i++ {
+		re := r[i] + l[n-i-1]
+		once.Do(func() {
+			min = re
+		})
+		if min > re {
+			min = re
+		}
+	}
+
+	fmt.Printf("%#v\n", min)
 }
 
 func SingleInt() int {
