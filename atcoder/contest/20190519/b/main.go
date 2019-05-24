75251624e5e565ea7dc1a15d43e2b20ff6a2566c
smith-30 <parkour12019@gmail.com>
2019-05-25 02:22:32 +0900

    add:

--- atcoder/contest/20190519/b/main.go
+++ atcoder/contest/20190519/b/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	var n string
+	fmt.Scan(&n)
+
+	nn := strings.Split(n, "")
+	a := nn[0] + nn[1]
+	b := nn[2] + nn[3]
+
+	c, _ := strconv.Atoi(a)
+	d, _ := strconv.Atoi(b)
+
+	switch {
+	case 0 < c && c <= 12 && 0 < d && d <= 12:
+		fmt.Println("AMBIGUOUS")
+	case (0 == c) || (0 == d || d > 12):
+		fmt.Println("NA")
+	case c > 12 && 0 < d && d <= 12:
+		fmt.Println("YYMM")
+	case d > 12 && 0 < c && c <= 12:
+		fmt.Println("MMYY")
+	}
 }
--- atcoder/contest/20190519/c/main.go
+++ atcoder/contest/20190519/c/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var n, k int
+	fmt.Scan(&n)
+	fmt.Scan(&k)
+
+	var re float64
+	for index := 1; index <= n; index++ {
+		b := 1 / float64(n)
+		if k-1 < index {
+			re += b
+			continue
+		}
+		logb := float64(k) / float64(index)
+		re += b * math.Pow(0.5, math.Ceil(math.Log2(logb)))
+	}
+
+	fmt.Printf("%v\n", re)
 }
