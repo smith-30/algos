9d6831ad6f7e8012c1c2c6e13a07109b960642fe
smith-30 <parkour12019@gmail.com>
2019-06-24 23:07:57 +0900

    add:

--- atcoder/contest/20190216/c/main.go
+++ atcoder/contest/20190216/c/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.ParseInt(sc.Text(), 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return int(i)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
 func main() {
 
 }
