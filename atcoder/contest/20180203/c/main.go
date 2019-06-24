27766a5d50db789a5def21bf3e02cd5e55b2ee6b
smith-30 <parkour12019@gmail.com>
2019-06-25 01:10:52 +0900

    mod:

--- atcoder/contest/20180203/c/main.go
+++ atcoder/contest/20180203/c/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.ParseInt(sc.Text(), 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return int(i)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	n, m := nextInt(), nextInt()
+
+	if n >= m {
+		fmt.Println(0)
+		return
+	}
+
+	vv := make([]int, 0, m)
+	for i := 0; i < m; i++ {
+		vv = append(vv, nextInt())
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(vv)))
+
+	nn := make([]int, 0, m)
+	for i, item := range vv {
+		if i+1 == len(vv) {
+			break
+		}
+		nn = append(nn, item-vv[i+1])
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(nn)))
+
+	var sum int
+	for _, item := range nn[n-1:] {
+		sum += item
+	}
+
+	fmt.Printf("%#v\n", sum)
 }
